250c39cb824b8bb2a515680c627a84f7130466c3
parba49 <iiuccsestudent@gmail.com>
2025-01-17 19:46:42 -0800

    HomrePage updating

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>hello</h1>
+        <div className=''>
+          <div className='bg-black border-l-purple-600 border-4'>
+            <h1 className=' text-white font-bold text-2xl'>Welcome to 
+           <span className='text-4xl text-amber-400'> Mess Management Web Application</span></h1>
+          </div>
+          <div className='grid grid-cols-2'>
+          <div>
+           <h1>hello</h1>
+           </div>
+           <div>
+           <h1>hello</h1>
+           </div>
+          </div>
         </div>
     );
 };
